760d054f4cf165bcc476c379c134aef03ae49638
mahfuzon <mahfuzon0@gmail.com>
2020-09-02 17:44:55 +0700

    mengambil informasi dari figma dan menambahkan shared value terbaru

--- lib/shared/shared.dart
+++ lib/shared/shared.dart
@@ -1,3 +1,6 @@
 import 'package:flutix/bloc/blocs.dart';
-
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+part 'theme.dart';
 part 'shared_value.dart';
