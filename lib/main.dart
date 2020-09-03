d0796844493bbeffa38486f4473da738b9b8ace3
mahfuzon <mahfuzon0@gmail.com>
2020-09-03 17:59:36 +0700

    Desain Login Page part 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutix/bloc/blocs.dart';
+import 'package:flutix/bloc/theme_bloc.dart';
 import 'package:flutix/services/services.dart';
 import 'package:flutix/ui/pages/pages.dart';
 import 'package:flutter/material.dart';
@@ -18,10 +19,14 @@ class MyApp extends StatelessWidget {
         providers: [
           BlocProvider(create: (_) => PageBloc()),
           BlocProvider(create: (_) => UserBloc()),
+          BlocProvider(create: (_) => ThemeBloc()),
         ],
-        child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: Wrapper(),
+        child: BlocBuilder<ThemeBloc, ThemeState>(
+          builder: (_, themeState) => MaterialApp(
+            theme: themeState.themeData,
+            debugShowCheckedModeBanner: false,
+            home: Wrapper(),
+          ),
         ),
       ),
     );
--- lib/ui/pages/sign_in_page.dart
+++ lib/ui/pages/sign_in_page.dart
@@ -6,18 +6,107 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  bool isEmailValid = false;
+  bool isPasswordValid = false;
+  bool isSignInIn = false;
   @override
   Widget build(BuildContext context) {
+    context
+        .bloc<ThemeBloc>()
+        .add(ChangeTheme(ThemeData().copyWith(primaryColor: accentColor2)));
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Sign In Page'),
-      ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Sign In'),
-          onPressed: () async {
-            AuthServices.signIn('kakasi@gmail.com', '1234567');
-          },
+      backgroundColor: Colors.white,
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+        child: ListView(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  height: 70,
+                  child: Image(
+                    image: AssetImage('assets/logo.png'),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 70, bottom: 40),
+                  child: Text(
+                    'Welcome Back,\nExplorer!:',
+                    style: blackTextFont.copyWith(fontSize: 20),
+                  ),
+                ),
+                TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      labelText: "Email Address",
+                      hintText: "Email Address"),
+                ),
+                Padding(padding: EdgeInsets.only(bottom: 16)),
+                TextField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      labelText: "Password",
+                      hintText: "Password"),
+                ),
+                Padding(padding: EdgeInsets.only(bottom: 6)),
+                Row(
+                  children: [
+                    Text(
+                      'Forgot Password?',
+                      style: greyTextFont.copyWith(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.only(right: 5)),
+                    Text('Get Now', style: purpleTextFont)
+                  ],
+                ),
+                Center(
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    margin: EdgeInsets.only(top: 40, bottom: 30),
+                    child: FloatingActionButton(
+                        child: Icon(
+                          Icons.arrow_forward,
+                        ),
+                        onPressed: () {},
+                        backgroundColor: mainColor),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Start Fresh Now?',
+                      style: greyTextFont.copyWith(fontWeight: FontWeight.w400),
+                    ),
+                    Padding(padding: EdgeInsets.all(5)),
+                    Text(
+                      'Sign Up',
+                      style: purpleTextFont,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
