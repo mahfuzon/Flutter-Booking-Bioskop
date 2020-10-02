19e306dd8ccd48aa4d8ceda8c60f5b8478de220d
mahfuzon <mahfuzon0@gmail.com>
2020-10-02 09:22:16 +0700

    MovieCard and RaitingStars widget

--- lib/ui/widgets/movie_card.dart
+++ lib/ui/widgets/movie_card.dart
@@ -1,10 +1,10 @@
 part of 'widgets.dart';
 
 class MovieCard extends StatelessWidget {
-  final Movie movies;
+  final Movie movie;
   final Function onTap;
 
-  MovieCard(this.movies, {this.onTap});
+  MovieCard(this.movie, {this.onTap});
 
   @override
   Widget build(BuildContext context) {
@@ -12,39 +12,36 @@ class MovieCard extends StatelessWidget {
       height: 140,
       width: 210,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        image: DecorationImage(
-          image: NetworkImage(imageBaseURL + "w700" + movies.backdropPath),
-        ),
-      ),
+          borderRadius: BorderRadius.circular(15),
+          image: DecorationImage(
+              image: NetworkImage(imageBaseURL + "w780" + movie.backdropPath),
+              fit: BoxFit.cover)),
       child: Container(
         height: 140,
         width: 210,
         padding: EdgeInsets.all(16),
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          gradient: LinearGradient(
-            colors: [
-              Colors.black.withOpacity(0.61),
-              Colors.black.withOpacity(0)
-            ],
-            begin: Alignment.bottomCenter,
-            end: Alignment.topCenter,
-          ),
-        ),
+            borderRadius: BorderRadius.circular(15),
+            gradient: LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                colors: [
+                  Colors.black.withOpacity(0.61),
+                  Colors.black.withOpacity(0)
+                ])),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
+          children: <Widget>[
             Text(
-              movies.title,
+              movie.title,
               style: whiteTextFont,
               maxLines: 2,
               overflow: TextOverflow.ellipsis,
             ),
-            RaitingStars(
-              voteAverage: movies.voteAverage,
-            ),
+            RatingStars(
+              voteAverage: movie.voteAverage,
+            )
           ],
         ),
       ),
