bb704c613a09718e65cc13cc28c02ae97eb57bc5
mahfuzon <mahfuzon0@gmail.com>
2020-12-01 09:39:17 +0700

    Checkout Page part 3

--- lib/ui/pages/select_seats_page.dart
+++ lib/ui/pages/select_seats_page.dart
@@ -1,43 +1,43 @@
-part of "pages.dart";
+part of 'pages.dart';
 
 class SelectSeatsPage extends StatefulWidget {
   final Ticket ticket;
 
   SelectSeatsPage(this.ticket);
+
   @override
   _SelectSeatsPageState createState() => _SelectSeatsPageState();
 }
 
 class _SelectSeatsPageState extends State<SelectSeatsPage> {
   List<String> selectedSeats = [];
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-      onWillPop: () async {
-        context
-            .bloc<PageBloc>()
-            .add(GoToSelectSchedulePage(widget.ticket.movieDetail));
-        return;
-      },
-      child: Scaffold(
-        body: Stack(
-          children: [
-            Container(
-              color: accentColor1,
-            ),
+        onWillPop: () async {
+          context
+              .bloc<PageBloc>()
+              .add(GoToSelectSchedulePage(widget.ticket.movieDetail));
+
+          return;
+        },
+        child: Scaffold(
+            body: Stack(
+          children: <Widget>[
+            Container(color: accentColor1),
             SafeArea(
               child: Container(color: Colors.white),
             ),
             ListView(
-              children: [
+              children: <Widget>[
                 Column(
-                  children: [
-                    // WIDGET: Header
+                  children: <Widget>[
+                    // note: HEADER
                     Row(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        // WIDGET: Arrow Back
+                      children: <Widget>[
                         Container(
                           margin: EdgeInsets.only(top: 20, left: defaultMargin),
                           padding: EdgeInsets.all(1),
@@ -53,136 +53,124 @@ class _SelectSeatsPageState extends State<SelectSeatsPage> {
                             ),
                           ),
                         ),
-                        // WIDGET: Poster dan Judul
                         Padding(
                           padding:
                               EdgeInsets.only(top: 20, right: defaultMargin),
                           child: Row(
-                            children: [
+                            children: <Widget>[
                               Container(
-                                margin: EdgeInsets.only(right: 16),
-                                width: MediaQuery.of(context).size.width * 0.5,
-                                child: Text(
-                                  widget.ticket.movieDetail.title,
-                                  style: blackTextFont.copyWith(fontSize: 20),
-                                  maxLines: 2,
-                                  overflow: TextOverflow.clip,
-                                  textAlign: TextAlign.end,
-                                ),
-                              ),
+                                  margin: EdgeInsets.only(right: 16),
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.5,
+                                  child: Text(
+                                    widget.ticket.movieDetail.title,
+                                    style: blackTextFont.copyWith(fontSize: 20),
+                                    maxLines: 2,
+                                    overflow: TextOverflow.clip,
+                                    textAlign: TextAlign.end,
+                                  )),
                               Container(
                                 width: 60,
                                 height: 60,
                                 decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(8),
-                                  image: DecorationImage(
-                                    image: NetworkImage(
-                                      imageBaseURL +
-                                          "w154" +
-                                          widget.ticket.movieDetail.posterPath,
-                                    ),
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                              ),
+                                    borderRadius: BorderRadius.circular(8),
+                                    image: DecorationImage(
+                                        image: NetworkImage(imageBaseURL +
+                                            'w154' +
+                                            widget
+                                                .ticket.movieDetail.posterPath),
+                                        fit: BoxFit.cover)),
+                              )
                             ],
                           ),
-                        ),
+                        )
                       ],
                     ),
-                    // WIDGET: Cinema Screen
+                    // note: CINEMA SCREEN
                     Container(
                       margin: EdgeInsets.only(top: 30),
                       width: 277,
                       height: 84,
                       decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage("assets/screen.png"),
-                        ),
-                      ),
+                          image: DecorationImage(
+                              image: AssetImage("assets/screen.png"))),
                     ),
-                    // WIDGET: SEATS
+                    // note: SEATS
                     generateSeats(),
-                    SizedBox(height: 30),
-                    // WIDGET: Button
-                    Column(
-                      children: [
-                        FloatingActionButton(
-                          onPressed: (selectedSeats != [])
-                              ? () {
-                                  context
-                                      .bloc<PageBloc>()
-                                      .add(GoToCheckoutPage(widget.ticket));
-                                }
-                              : null,
+                    // note: NEXT BUTTON
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: FloatingActionButton(
                           elevation: 0,
-                          backgroundColor: (selectedSeats.length > 0)
+                          backgroundColor: selectedSeats.length > 0
                               ? mainColor
                               : Color(0xFFE4E4E4),
                           child: Icon(
                             Icons.arrow_forward,
-                            color: (selectedSeats.length > 0)
+                            color: selectedSeats.length > 0
                                 ? Colors.white
                                 : Color(0xFFBEBEBE),
                           ),
-                        ),
-                      ],
+                          onPressed: selectedSeats.length > 0
+                              ? () {
+                                  context.bloc<PageBloc>().add(GoToCheckoutPage(
+                                      widget.ticket
+                                          .copyWith(seats: selectedSeats)));
+                                }
+                              : null),
                     ),
-                    SizedBox(height: 30),
+                    SizedBox(
+                      height: 50,
+                    ),
                   ],
-                ),
+                )
               ],
-            ),
+            )
           ],
-        ),
-      ),
-    );
+        )));
   }
 
   Column generateSeats() {
-    List<int> numberOfSeats = [3, 5, 5, 5, 5];
+    List<int> numberofSeats = [3, 5, 5, 5, 5];
     List<Widget> widgets = [];
-    for (int i = 0; i < numberOfSeats.length; i++) {
-      widgets.add(
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: List.generate(
-            numberOfSeats[i],
+
+    for (int i = 0; i < numberofSeats.length; i++) {
+      widgets.add(Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: List.generate(
+            numberofSeats[i],
             (index) => Padding(
-              padding: EdgeInsets.only(
-                right: (index < numberOfSeats.length - 1) ? 16 : 0,
-                bottom: 16,
-              ),
-              child: SelectableBox(
-                // baris dilihat dari variabel yang digunakan untuk perulangan
-                // sedangkan kolom dilihat dari index nya
-                // mendapatkan isi dari selectable box menggunakan code asci
-                "${String.fromCharCode(i + 65)}${index + 1}",
-                width: 40,
-                height: 40,
-                textStyle: blackNumberFont,
-                isSelected: selectedSeats
-                    .contains("${String.fromCharCode(i + 65)}${index + 1}"),
-                onTap: () {
-                  String seatNumber =
-                      "${String.fromCharCode(i + 65)}${index + 1}";
-                  setState(
-                    () {
-                      if (selectedSeats.contains(seatNumber)) {
-                        selectedSeats.remove(seatNumber);
-                      } else {
-                        selectedSeats.add(seatNumber);
-                      }
+                  padding: EdgeInsets.only(
+                      right: index < numberofSeats[i] - 1 ? 16 : 0, bottom: 16),
+                  child: SelectableBox(
+                    "${String.fromCharCode(i + 65)}${index + 1}",
+                    width: 40,
+                    height: 40,
+                    textStyle: whiteNumberFont.copyWith(color: Colors.black),
+                    isSelected: selectedSeats
+                        .contains("${String.fromCharCode(i + 65)}${index + 1}"),
+                    onTap: () {
+                      String seatNumber =
+                          "${String.fromCharCode(i + 65)}${index + 1}";
+                      setState(() {
+                        if (selectedSeats.contains(seatNumber)) {
+                          selectedSeats.remove(seatNumber);
+                        } else {
+                          selectedSeats.add(seatNumber);
+                        }
+                      });
                     },
-                  );
-                },
-                isEnabled: index != 0,
-              ),
-            ),
-          ),
-        ),
-      );
+                    isEnabled: index != 0,
+                  ),
+                )),
+      ));
     }
-    return Column(children: widgets);
+
+    return Column(
+      children: widgets,
+    );
   }
 }
